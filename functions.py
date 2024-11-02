7f3711848c0ec0b5f273d199e2806497ce92ddba
yangibal <95936026+yangibal@users.noreply.github.com>
2024-11-02 15:39:17 +0100

    final commit

--- functions.py
+++ functions.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 import graphviz 
+import sklearn.metrics as metrics
 from sklearn import tree
 
 def classComparePlot(df, class_name, plotType='density'):
@@ -209,3 +210,18 @@ def find_outliers(feature):
 
     # Return rows where the feature is outside the outlier boundaries
     return np.where((feature > upper_bound) | (feature < lower_bound))
+
+def regression_results(y_true, y_pred):
+
+    # Regression metrics
+    explained_variance=metrics.explained_variance_score(y_true, y_pred)
+    mean_absolute_error=metrics.mean_absolute_error(y_true, y_pred) 
+    mse=metrics.mean_squared_error(y_true, y_pred) 
+    median_absolute_error=metrics.median_absolute_error(y_true, y_pred)
+    r2=metrics.r2_score(y_true, y_pred)
+
+    print('explained_variance: ', round(explained_variance,4))    
+    print('r2: ', round(r2,4))
+    print('MAE: ', round(mean_absolute_error,4))
+    print('MSE: ', round(mse,4))
+    print('RMSE: ', round(np.sqrt(mse),4))
